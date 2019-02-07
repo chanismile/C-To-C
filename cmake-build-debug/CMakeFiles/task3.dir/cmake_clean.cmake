c72062523421ec0381c4997574c5081b2922a5ed
chanismile <37192892+chanismile@users.noreply.github.com>
2019-02-08 01:46:36 +0200

    uploading the project

---
+++ cmake-build-debug/CMakeFiles/task3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task3.dir/main.c.o"
+  "CMakeFiles/task3.dir/poly.c.o"
+  "task3.pdb"
+  "task3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
